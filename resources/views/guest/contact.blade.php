6ad2051b00337fc7f2fed5110e0752878fd0052f
ShyamCSE <shyamyadav.cse@gmail.com>
2023-02-13 10:46:52 +0530

    contact map remove

--- resources/views/guest/contact.blade.php
+++ resources/views/guest/contact.blade.php
@@ -90,11 +90,7 @@
               </div>
             </div>
 
-            <div class="col-lg-12">
-              <div id="map">
-                <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
-              </div>
-            </div>
+     
 
           </div>
         </div>
